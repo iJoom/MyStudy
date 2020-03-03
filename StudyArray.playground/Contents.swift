018e67d4de87d1d6cd65786b22025feb8db9235b
iJoom <ghfdld@naver.com>
2020-03-03 15:55:58 +0900

    add simple array study

---
+++ StudyArray.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var ary: [Int] = []
+
+ary.append(0)
+ary.append(2)
+
+print("\(ary[0]) \(ary[1])")
+
+struct test {
+    var value: Int
+}
+
+var ary2: [Int] = [1,2,3,4,5]
+// element와 index를 가지고 있는 배열
+var ary3 = [test(value: 0), test(value: 3), test(value: 5)]
+//구조체를 가지는 가변배열
+ary2[0] = 1204
+
+print(ary2[0])
+
+print(ary3[0])
+
+
+
+
