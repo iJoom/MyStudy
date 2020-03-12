84ea2be075a28832221647f9895e89d65ea331e0
iJoom <ghfdld@naver.com>
2020-03-12 13:43:46 +0900

    add 7th study

--- StudyAlgorithm/StudyAlgorithm/main.cpp
+++ StudyAlgorithm/StudyAlgorithm/main.cpp
@@ -1,25 +1,38 @@
-//
-//  main.cpp
-//  StudyAlgorithm
-//
-//  Created by IJ . on 2020/03/11.
-//  Copyright © 2020 jun. All rights reserved.
-//
-
 #include <iostream>
 #include <vector>
 #include <algorithm>
 using namespace std;
 int main() {
-    // insert code here...
-    vector<int> a;
+    char input[100];
     
-    a.push_back(1);
-    a.push_back(2);
-    
-    sort(a.begin(), a.end(),greater<int>());
-    
-    cout << "Hello, World!\n"<<a[0]<<"      "<<a[1];
-    printf("\n a[0]값 = %d\n",a[0]);
+    int N; // 2~100000
+    int K; // 2~N
+    cin>>N>>K;
+//    printf("%d %d\n",N,K);
+    fflush(stdin);
+    char ary[N*2];
+    char p;
+    string ak;
+        cin.getline(ary,N*2);
+//        cout<<ary<<endl;
+    string str(ary);//char배열 -> string으로
+//   str.erase(remove(str.begin(), str.end(), isspace), str.end());
+   str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    char ch[N];
+    strcpy(ch, str.c_str());
+//    cout<<ch<<endl;
+//    cout<<"공백제거문자열:"<<str<<endl;
+    int smallest=ch[0];
+    int cnt = 0;
+    for(int i=1;i<N;i++){
+        if(ch[i]<smallest){
+            smallest=ch[i];
+        }
+    }
+    while (N>1) {
+        N -= (K -1);
+        cnt++;
+    }
+    cout<<cnt;
     return 0;
 }
