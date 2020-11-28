073977ad12dcc18456449c00617c4f2b64c9e685
iJoom <ghfdld@naver.com>
2020-11-28 23:08:59 +0900

    lazy 공부 ,

--- SmartAppProgram/CH6/CH6/SwitchingViewController.swift
+++ SmartAppProgram/CH6/CH6/SwitchingViewController.swift
@@ -14,6 +14,7 @@ class SwitchingViewController: UIViewController {
         case table1
         case table2
     }
+    lazy var abc: Int = 2
     
     
     private var blueViewController: BlueViewController!
