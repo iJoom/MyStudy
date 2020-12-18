197fcb6dda3dda1ff7537004853855eb24986784
iJoom <ghfdld@naver.com>
2020-12-18 19:06:45 +0900

    create TableViewCell indexPath delegate Test

---
+++ SmartAppProgram/CH7/CH7/CH7/TestTableViewCell.swift
@@ -0,0 +1,50 @@
+//
+//  TestTableViewCell.swift
+//  CH7
+//
+//  Created by IJ . on 2020/12/17.
+//
+
+import UIKit
+
+protocol DeleteButtonDelegate: AnyObject{
+    func shouldTouchDeleteButton(at indexPath: IndexPath)
+    
+    func shouldTouchDeleteButton(at cell: TestTableViewCell)
+    
+}
+
+class TestTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var deleteButton: UIButton!
+    weak var delegate: DeleteButtonDelegate?
+    
+    var indexPath2: IndexPath?
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    @IBAction func touchUpDeleteButton(_ sender: UIButton) {
+        
+//        if let superView = self.superview as? UITableView {
+//            print("cell sender indexPath 값: \(indexPath2)")
+//            
+//            let indexPath = superView.indexPath(for: self)
+//            print(indexPath)
+//            
+//            self.delegate?.shouldTouchDeleteButton(at: indexPath!)
+//        } else {
+//            print("superview is not a UITableView - getIndexPath")
+//        }
+        
+        self.delegate?.shouldTouchDeleteButton(at: self)
+       
+    }
+}
--- SmartAppProgram/CH7/CH7/CH7/ViewController.swift
+++ SmartAppProgram/CH7/CH7/CH7/ViewController.swift
@@ -9,11 +9,75 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testTableView: UITableView!
+    
+    class bb {
+        func printff() {
+          print("asdf")
+        }
+    }
+    var rowCount: Int = 5
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        testTableView.delegate = self
+        testTableView.dataSource = self
+        
+      
     }
 
 
 }
 
+
+extension ViewController:UITableViewDelegate,UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 130
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return rowCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "testCell") as! TestTableViewCell
+        cell.delegate = self
+        
+    
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: DeleteButtonDelegate {
+    func shouldTouchDeleteButton(at cell: TestTableViewCell) {
+        
+        let indexPath = testTableView.indexPath(for: cell)
+        print("델리게이트 indexPath값 \(indexPath)")
+       
+        if let indexPaths = indexPath {
+            rowCount -= 1
+            self.testTableView.deleteRows(at: [indexPaths], with: .fade)
+        }
+        
+    }
+    
+    
+    
+    func shouldTouchDeleteButton(at indexPath: IndexPath) {
+        
+//        print("뷰컨 indexPath값 \(indexPath)")
+//        rowCount -= 1
+//        self.testTableView.deleteRows(at: [indexPath], with: .fade)
+       //numberOfRowInSection 업데이트가 먼저되고 delete되야 쓰레드 에러 발생 X
+        
+    }
+    
+    
+    
+    
+}
