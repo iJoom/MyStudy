e09ff33b2ea274ab11d21fae80d96aafdf8ff4a9
iJoom <ghfdld@naver.com>
2020-12-21 23:22:17 +0900

    create 7 ch picker project

---
+++ SmartAppProgram/CH7/Picker/Picker/DatePickerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DatePickerViewController.swift
+//  Picker
+//
+//  Created by IJ . on 2020/12/21.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
---
+++ SmartAppProgram/CH7/Picker/Picker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Picker
+//
+//  Created by IJ . on 2020/12/21.
+//
+
+import UIKit
+
+class ViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
