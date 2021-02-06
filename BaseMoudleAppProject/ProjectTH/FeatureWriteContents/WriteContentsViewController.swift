70e89f634c31a8653ed4433aa7bf8a529a910f15
iJoom <ghfdld@naver.com>
2021-02-06 18:07:08 +0900

    back up base moudle project

---
+++ BaseMoudleAppProject/ProjectTH/FeatureWriteContents/WriteContentsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FeatureWriteContents
+//
+//  Created by IJ . on 2021/02/06.
+//
+
+import UIKit
+
+class WriteContentsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BaseMoudleAppProject/ProjectTH/ProjectTH/HomeTabViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjectTH
+//
+//  Created by IJ . on 2021/02/06.
+//
+
+
+import UIKit
+import FeatureTest1
+
+
+
+class HomeTabViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+
+}
+
