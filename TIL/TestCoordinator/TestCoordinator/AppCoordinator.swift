9cefbe6a105ddbedc69d86e6b7fd660330876dfd
iJoom <ghfdld@naver.com>
2021-08-05 01:19:32 +0900

    add casting private property with func

--- TIL/TestCoordinator/TestCoordinator/AppCoordinator.swift
+++ TIL/TestCoordinator/TestCoordinator/AppCoordinator.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 final class AppCoordinator {
-    private var rvc: UINavigationController? {
+    private var windowRootViewController: UINavigationController? {
         return window.rootViewController as? UINavigationController
     }
     private let window: UIWindow
@@ -19,7 +19,6 @@ final class AppCoordinator {
     }
     
     func start() {
-        
         onLogIn = false
         
         if onLogIn ?? false {
@@ -31,19 +30,30 @@ final class AppCoordinator {
             window.rootViewController = navigationController
         }
         
-        
         window.makeKeyAndVisible()
     }
     
     //다른View에서 Coordinator를 이용해 View 전환하는 법 고려해보기
-    //Coordinator 싱글턴... 흠?
     
     func moveToSecondView() {
         let secondViewController = SecondViewViewController.instantiate(TestViewModel(),coordiNator: self)
-//        window.rootViewController.pushViewController(secondViewController, animated: true)
         let secondView = window.rootViewController as? UINavigationController
-            secondView?.pushViewController(secondViewController, animated: true)
+        secondView?.pushViewController(secondViewController, animated: true)
         
         print("moveToSecondView")
+    }
+    
+    func moveToThirdView() {
+        let thirdViewController = ThirdViewController.instantiate(TestViewModel(), coordiNator: self)
+        let rootView = window.rootViewController as? UINavigationController
+        if let rv = windowRootViewController {
+            rv.pushViewController(thirdViewController, animated: true)
+        }
+        print("moveToThirdView")
+    }
+    
+    func popViewController() {
+        let rootView = window.rootViewController as? UINavigationController
+        rootView?.popViewController(animated: true)
     }
 }
