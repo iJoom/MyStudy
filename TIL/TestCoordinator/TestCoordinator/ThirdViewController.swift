05fd25ca60b6e97643c0ef39ce1f7ea3eda1c10d
iJoom <ghfdld@naver.com>
2021-08-05 01:19:57 +0900

    create test third view

--- TIL/TestCoordinator/TestCoordinator/ThirdViewController.swift
+++ TIL/TestCoordinator/TestCoordinator/ThirdViewController.swift
@@ -9,13 +9,35 @@ import UIKit
 
 class ThirdViewController: UIViewController {
 
+    private var viewModel: TestViewModel?
+    private weak var coordiNator: AppCoordinator?
+    //static func로 ViewModel VC생성과 함께 초기화
+    static func instantiate(_ viewModel: TestViewModel,coordiNator: AppCoordinator ) -> UIViewController {
+        let secondStoryBoard = UIStoryboard(name: "ThirdStoryboard", bundle: nil)
+        guard let thirdViewController = secondStoryBoard.instantiateViewController(identifier: "ThirdView") as? ThirdViewController else {
+            return UIViewController()
+        }
+        thirdViewController.viewModel = viewModel
+        thirdViewController.coordiNator = coordiNator
+        return thirdViewController
+    }
+    
+    deinit {
+        print("Third View Disappear")
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        
+    }
+    @IBAction func touchUpBackButton(_ sender: Any) {
+        coordiNator?.popViewController()
+    }
     
-
     /*
     // MARK: - Navigation
 
